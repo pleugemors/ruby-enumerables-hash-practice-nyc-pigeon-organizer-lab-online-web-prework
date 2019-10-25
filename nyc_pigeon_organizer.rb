3144b95f5c3bebfb385559997bb241a6be392f6e
Paul Leugemors <pleugemors@yahoo.com>
2019-10-25 05:02:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,24 +13,24 @@ def nyc_pigeon_organizer(data)
   
   row = 0
   while row < key1_name.size do 
-    values = data[key1_name[row]].keys.to_s
+    values = data[key1_name[row]].keys
     row2 = 0 
     while row2 < values.size do 
-      name_array = data[key1_name[row]][values[row2]].to_s
+      name_array = data[key1_name[row]][values[row2]]
       row3 = 0 
       while row3 < name_array.size do 
         if info_hash.has_key?(name_array[row3])
           if info_hash[name_array[row3]].has_key?(key1_name[row])
             if info_hash[name_array[row3]][key1_name[row]]== values[row2]
             else
-              info_hash[name_array[row3]][key1_name[row]] << values[row2]
+              info_hash[name_array[row3]][key1_name[row]] << "#{values[row2]}"
             end
           else
-            info_hash[name_array[row3]][key1_name[row]]= values[row2]
+            info_hash[name_array[row3]][key1_name[row]]= "#{values[row2]}"
           end
         else
           info_hash[name_array[row3]]= {key1_name[row] => []}
-          info_hash[name_array[row3]][key1_name[row]] << values[row2]
+          info_hash[name_array[row3]][key1_name[row]] << "#{values[row2]}"
         end
         row3 += 1 
       end
